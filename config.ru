a6ac0bd3d4b0f73aedc3f19e6a05894b12daeede
bwidlak <bartekwidlak@gmail.com>
2013-05-28 18:23:55 +0100

    rest of the files for i18n

--- config.ru
+++ config.ru
@@ -1,2 +1,6 @@
 require './server'
+
+I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'config', 'locales', '*.yml').to_s]
+I18n.default_locale = "en"
+
 run Sinatra::Application
--- server.rb
+++ server.rb
@@ -1,10 +1,18 @@
 require 'sinatra'
 require 'haml'
+require 'i18n'
 
 configure :production do
   require 'newrelic_rpm'
 end
 
 get '/' do
-  haml :index
+	I18n.locale = params[:locale]
+	haml :index
+end
+
+helpers do
+	def t(*args)
+		I18n.t(*args)
+	end
 end
